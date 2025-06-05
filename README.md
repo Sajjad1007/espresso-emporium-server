1435c152e4d869e6eeac64aa1dfdfe52bbae996b
Sajjadur Rahman <sajjadurrahman5561@gmail.com>
2025-06-05 12:44:18 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+### IN THE NAME OF ALLAH, THE MOST GRACIOUS, THE MOST MERCIFUL
+
+# Espresso Emporium
+
+Welcome to the server of **Espresso Emporium**, a modern and fully responsive website designed to showcase the premium coffees of a coffee shop. Along with the [client](https://github.com/Sajjad1007/espresso-emporium-client), this project aims to provide an engaging and seamless user experience, ensuring a visually appealing layout with easy accessibility on mobile, tablet and desktop devices.
+
+### [Click here](https://espresso-emporium-server-6xb3fu8f6-sajjad1007s-projects.vercel.app/) to visit the server.
+
+## Technologies Used
+
+1. **Node.js:** JavaS runtime environment for building the backend server.
+2. **Express.js:** Node.js framework used to build the RESTful API and handle routing.
+3. **MongoDB:** NoSQL database used to store and manage coffee product data.
+4. **Cors:** Middleware to enable cross-origin requests between client and server.
+5. **Dotenv:** Module to securely manage environment variables like database credentials.
---
+++ index.js
@@ -0,0 +1,95 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Espresso Emporium server is getting hotter.");
+});
+
+app.listen(port, () => {
+  console.log(`Espresso Emporium is running on port ${port}.`);
+});
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster01.ezzxwt4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster01`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const coffeesCollection = client.db("coffeeDB").collection("coffees");
+    const usersCollection = client.db("coffeeDB").collection("users");
+
+    app.get("/coffees/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await coffeesCollection.findOne(query);
+      res.send(result);
+    });
+
+    app.get("/coffees", async (req, res) => {
+      const result = await coffeesCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.post("/coffees", async (req, res) => {
+      const newCoffee = req.body;
+      const result = await coffeesCollection.insertOne(newCoffee);
+      res.send(result);
+    });
+
+    app.put("/coffees/:id", async (req, res) => {
+      const id = req.params.id;
+      const updatedCoffee = req.body;
+      const query = { _id: new ObjectId(id) };
+      const updatedDoc = {
+        $set: updatedCoffee,
+      };
+      const result = await coffeesCollection.updateOne(query, updatedDoc);
+      res.send(result);
+    });
+
+    app.delete("/coffees/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await coffeesCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    app.get("/users", async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.post("/users", async (req, res) => {
+      const newUser = req.body;
+      const result = await usersCollection.insertOne(newUser);
+      res.send(result);
+    });
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
